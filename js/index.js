65769f98a350e66c06996d7021e5362da00bdc54
Rodriques <rodriques.perry@gmail.com>
2021-01-01 16:39:57 -0600

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// // Typewriter Script
+// let i = 0;
+// let txt = "";
+// let speed = 150;
+
+// function type() {
+//   document.getElementById("type").innerText += txt.charAt(i);
+//   i++;
+//   setTimeout(type, speed);
+// }
+// type();
+
